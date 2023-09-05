47d4f2ea591b8f44a6fc954de228d6631809de5a
Patipol Chat <patipolchat@gmail.com>
2023-09-05 13:30:14 +0700

    Move In to a go package

--- convert/convert.go
+++ convert/convert.go
@@ -1,4 +1,4 @@
-package go_bahttext
+package convert
 
 import (
 	"errors"
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
-package go_bahttext
+package main
 
 import (
 	"fmt"
+
+	convert "github.com/patipolchat/go-bahttext/convert"
 )
 
 func main() {
 	exampleAmount := "7,532.73 บาท"
-	result, err := ConvertToText(exampleAmount)
+	result, err := convert.ConvertToText(exampleAmount)
 	if err != nil {
 		panic(err)
 	}
